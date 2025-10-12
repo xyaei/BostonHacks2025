cbc1e1f83396224f9b7737d910a7367c4167e2e3
Kate Eads <kateeads@crc-dot1x-nat-10-239-20-45.bu.edu>
2025-10-11 20:00:36 -0400

    added desktop app

--- desktop-app/dist/petRenderer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import PetDisplay from './components/PetDisplay.jsx';
-import './index.css';
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(/*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(PetDisplay, null)));
--- desktop-app/dist/popupRenderer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { InterventionPopup } from './components/InterventionPopup';
-import './index.css';
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(/*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(InterventionPopup, null)));
